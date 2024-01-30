1612854091ca55c7ef1ec0961fbf012ab40c7f4b
ik04 <ishaankhurana123@gmail.com>
2024-01-30 11:26:39 +0530

    feat: setup user functions in server, test connect event in frontend

--- server/rest/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/rest/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string("wallet_address")->index()->nullable();
+            $table->string("wallet_address")->index()->unique();
             $table->timestamps();
         });
     }
